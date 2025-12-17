93261f225fb8e836d00c78094eb6aa04a176f3be
Daniel Hogan <timtheonly@gmail.com>
2025-12-17 12:13:35 +0000

    add validation for input

--- app/models/short_url.py
+++ app/models/short_url.py
@@ -1,6 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, AfterValidator
+from typing import Annotated
+import validators
+
+
+def is_valid_url(value: str) -> str:
+    result = validators.url(value)
+    if not result:
+        raise ValueError(f"Invalid url value {result}")
+    return value
+
+
+def is_valid_short_id(value: str) -> str:
+    if not value.isalnum() or len(value) < 3:
+        raise ValueError(
+            "Short url id must be greater than 3 characters and alphanumeric"
+        )
+    return value
 
 
 class ShortUrl(BaseModel):
-    short_url_id: str
-    long_url: str
+    short_url_id: Annotated[str, AfterValidator(is_valid_short_id)]
+    long_url: Annotated[str, AfterValidator(is_valid_url)]
